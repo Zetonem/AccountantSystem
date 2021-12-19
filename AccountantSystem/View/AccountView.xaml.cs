a1c8a1b6c0fd5f1dfa16c5c9032d85719a5a6a8a
Leonid Zaytsev <Leonid_Zaytsev@Dell.com>
2021-12-20 02:04:04 +0300

    Добавьте файлы проекта.

---
+++ AccountantSystem/View/AccountView.xaml.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace AccountantSystem.View
+{
+    /// <summary>
+    /// Логика взаимодействия для AccountView.xaml
+    /// </summary>
+    public partial class AccountView : UserControl
+    {
+        public AccountView()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            string messageBoxText;
+            MessageBoxImage icon;
+            string caption = "Регистрация данных";
+            MessageBoxButton button = MessageBoxButton.OK;
+            if (Text1.Text == null || Text2.Text == null || Text3.Text == null || Text4.Text == null ||
+                Text1.Text == String.Empty || Text2.Text == String.Empty || Text3.Text == String.Empty || Text4.Text == String.Empty)
+            {
+                messageBoxText = "Заполните все поля для регистрации.";
+                icon = MessageBoxImage.Warning;
+                MessageBox.Show(messageBoxText, caption, button, icon, MessageBoxResult.Yes);
+                return;
+            }
+            messageBoxText = "Регистрация прошла успешно.";
+            icon = MessageBoxImage.Information;
+
+            MessageBox.Show(messageBoxText, caption, button, icon, MessageBoxResult.Yes);
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            string messageBoxText = "Оплачено.";
+            string caption = "Оплата";
+            MessageBoxButton button = MessageBoxButton.OK;
+            MessageBoxImage icon = MessageBoxImage.Information;
+
+            MessageBox.Show(messageBoxText, caption, button, icon, MessageBoxResult.Yes);
+        }
+    }
+}
---
+++ AccountantSystem/View/ProxyView.xaml.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace AccountantSystem.View
+{
+    public partial class ProxyView : UserControl
+    {
+        private bool isButton0 = false, isButton1 = false, isButton2 = false, isButton3 = false;
+        public ProxyView()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            statuslabel.Visibility = Visibility.Visible;
+            statuslabel.Content = "Поручение получено";
+            statuslabel.Background = new SolidColorBrush(Color.FromRgb(0, 255, 0));
+            isButton0 = true;
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            if (!isButton0)
+            {
+                statuslabel.Visibility = Visibility.Visible;
+                statuslabel.Content = "Поручение не получено";
+                statuslabel.Background = new SolidColorBrush(Color.FromRgb(255, 0, 0));
+                return;
+            }
+            statuslabel.Content = "Оплата проверена";
+            statuslabel.Background = new SolidColorBrush(Color.FromRgb(0, 255, 0));
+            isButton1 = true;
+        }
+
+        private void Button_Click_2(object sender, RoutedEventArgs e)
+        {
+            if (!isButton1)
+            {
+                statuslabel.Visibility = Visibility.Visible;
+                statuslabel.Content = "Оплата не проверена";
+                statuslabel.Background = new SolidColorBrush(Color.FromRgb(255, 0, 0));
+                return;
+            }
+            if (sumTextBox.Text == String.Empty || numberTextBox.Text == String.Empty || nameTextBox.Text == String.Empty ||
+                sumTextBox.Text == null || numberTextBox.Text == null || nameTextBox.Text == null)
+            {
+                statuslabel.Content = "Невозможно сформировать доверенность. Заполните все поля";
+                statuslabel.Background = new SolidColorBrush(Color.FromRgb(255, 0, 0));
+                return;
+            }
+
+            statuslabel.Content = $"Сформирована доверенность #{numberTextBox.Text} на сумму {sumTextBox.Text} на сотрудника {nameTextBox.Text}";
+            statuslabel.Background = new SolidColorBrush(Color.FromRgb(0, 255, 0));
+            isButton2 = true;
+        }
+
+        private void Button_Click_3(object sender, RoutedEventArgs e)
+        {
+            if (!isButton2)
+            {
+                statuslabel.Visibility = Visibility.Visible;
+                statuslabel.Content = "Доверенность не сформирована";
+                statuslabel.Background = new SolidColorBrush(Color.FromRgb(255, 0, 0));
+                return;
+            }
+            statuslabel.Content = $"Доверенность #{numberTextBox.Text} заверена";
+            statuslabel.Background = new SolidColorBrush(Color.FromRgb(0, 255, 0));
+            isButton3 = true;
+        }
+
+        private void Button_Click_4(object sender, RoutedEventArgs e)
+        {
+            if (!isButton3)
+            {
+                statuslabel.Visibility = Visibility.Visible;
+                statuslabel.Content = "Доверенность не заверена";
+                statuslabel.Background = new SolidColorBrush(Color.FromRgb(255, 0, 0));
+                return;
+            }
+            statuslabel.Content = $"Поручение #{numberTextBox.Text} выдано";
+            statuslabel.Background = new SolidColorBrush(Color.FromRgb(0, 255, 0));
+            sumTextBox.Text = String.Empty;
+            numberTextBox.Text = String.Empty;
+            nameTextBox.Text = String.Empty;
+            isButton0 = false;
+            isButton1 = false;
+            isButton2 = false;
+            isButton3 = false;
+        }
+    }
+}
